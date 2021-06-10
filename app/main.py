da5e66e29241ed3d6f8584e7d7bed10bb662f330
Hoang Tran <tnhoang088@gmail.com>
2021-06-10 20:30:34 +0700

    chore: update app title

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
+from app.core.config import Settings
 from fastapi import FastAPI
 from app.api.api_v1.api import api_router
 
-app = FastAPI()
+app = FastAPI(title=Settings.PROJECT_NAME)
 
 app.include_router(api_router, prefix="/api/v1")
