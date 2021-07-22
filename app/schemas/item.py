f03d39de5e954d597b9766f0761cf5c18ec7426f
tnhoang <tnhoang088@gmail.com>
2021-07-22 16:55:19 +0700

    Add Item schema

---
+++ app/schemas/item.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+# Shared properties
+class ItemBase(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+
+
+# Properties to receive on item creation
+class ItemCreate(ItemBase):
+    title: str
