91ee9cd381dd708dd7becee091898a1d1f29bc45
tnhoang <tnhoang088@gmail.com>
2021-08-30 20:18:52 +0700

    feat: CRUD Item test cases

--- app/tests/crud/test_items.py
+++ app/tests/crud/test_items.py
@@ -1,7 +1,46 @@
+from app.crud.crud_item import CRUDItem
+from app.models.item import Item
+from app.schemas.item import ItemCreate
 from app.tests.factories.item import ItemFactory
 
+from .test_base_crud import TestCreate, TestDelete, TestGet, TestGetMulti, TestUpdate
 
-def test_item_factory():
-    item1 = ItemFactory()
-    item2 = ItemFactory()
-    assert item1.title == item2.title
+
+class TestItemGet(TestGet):
+    __test__ = True
+    model = Item
+    schema = ItemCreate
+    factory = ItemFactory
+    crud = CRUDItem
+
+
+class TestItemCreate(TestCreate):
+    __test__ = True
+    model = Item
+    schema = ItemCreate
+    factory = ItemFactory
+    crud = CRUDItem
+
+
+class TestItemUpdate(TestUpdate):
+    __test__ = True
+    model = Item
+    schema = ItemCreate
+    factory = ItemFactory
+    crud = CRUDItem
+
+
+class TestItemDelete(TestDelete):
+    __test__ = True
+    model = Item
+    schema = ItemCreate
+    factory = ItemFactory
+    crud = CRUDItem
+
+
+class TestItemGetMulti(TestGetMulti):
+    __test__ = True
+    model = Item
+    schema = ItemCreate
+    factory = ItemFactory
+    crud = CRUDItem
