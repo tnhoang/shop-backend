e235ad312ac5223e7594da9eb7d0ab0dc8a3722b
tnhoang <tnhoang088@gmail.com>
2021-09-03 09:46:04 +0700

    db migration

--- app/models/user.py
+++ app/models/user.py
@@ -7,8 +7,7 @@ from app.db.base_class import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    full_name = Column(String, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String, index=True)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
